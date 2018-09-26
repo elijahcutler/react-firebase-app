d3c481724fd3b78c32ee6778451665e48909a8f0
Elijah Cutler <elijahcutler@gmail.com>
2018-09-26 00:39:18 -0400

    Started work on adding routes

--- src/App.js
+++ src/App.js
@@ -1,19 +1,30 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { BrowserRouter as Router, Switch, Redirect } from "react-router-dom";
+import logo from "./logo.svg";
+import "./App.css";
+import Splash from "./Components/Splash";
+import Login from "./Components/Login";
+import Signup from "./Components/Signup";
+import Home from "./Components/Home";
+import Settings from "./Components/Settings";
+import { PublicRoute, PrivateRoute, DynamicRoute } from "./Components/Routes";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <Router>
+        <div>
+          <Switch>
+            <DynamicRoute
+              path="/"
+              authedComponent={props => <Home />}
+              unauthedComponent={props => <Splash />}
+            />
+            <PublicRoute exact path="/login" component={props => <Login />} />
+            <PublicRoute exact path="/signup" component={props => <Signup />} />
+          </Switch>
+        </div>
+      </Router>
     );
   }
 }
---
+++ src/Components/Splash.js
@@ -0,0 +1,23 @@
+import React, { Component } from "react";
+
+export default class extends Component {
+  render() {
+    return (
+      <div>
+        <div>
+          <h1>
+            Welcome to<br />FireChat
+          </h1>
+          <h2>Chat with peers about whatever you want.</h2>
+        </div>
+        <div>
+          <button>Sign Up</button>
+          <button>Login</button>
+        </div>
+        <div>
+          <h4>Privacy Policy</h4>
+        </div>
+      </div>
+    );
+  }
+}
